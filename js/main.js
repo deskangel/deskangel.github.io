3df9ef8fa1ee23795d67cfd994ebd5363288b18a
iDeskangel <williamx@deskangel.com>
2024-04-10 22:15:32 +0800

    * update

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@ if (!!$.prototype.justifiedGallery) {
   $(".article-gallery").justifiedGallery(options);
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   /**
    * Shows the responsive navigation menu on mobile.
    */
-  $("#header > #nav > ul > .icon").click(function() {
+  $("#header > #nav > ul > .icon").click(function () {
     $("#header > #nav > ul").toggleClass("responsive");
   });
 
@@ -34,13 +34,13 @@ $(document).ready(function() {
      */
     if ($(document).width() >= 1440) {
       menu.show();
-      menuIcon.addClass("active");
     }
+    menuIcon.addClass("active");
 
     /**
      * Display the menu if the menu icon is clicked.
      */
-    menuIcon.click(function() {
+    menuIcon.click(function () {
       if (menu.is(":hidden")) {
         menu.show();
         menuIcon.addClass("active");
@@ -55,11 +55,16 @@ $(document).ready(function() {
      * Add a scroll listener to the menu to hide/show the navigation links.
      */
     if (menu.length) {
-      $(window).on("scroll", function() {
-        var topDistance = menu.offset().top;
+      $(window).on("scroll", function () {
+        var topDistance = document.documentElement.scrollTop;// menu.offset().top;
 
         // hide only the navigation links on desktop
-        if (!nav.is(":visible") && topDistance < 100) {
+        // use 150 to fix the issue causing the nav never show again.
+        // var distanceLimit = 50;
+        // if ($(document).width() >= 1200) {
+        //   distanceLimit = 150;
+        // }
+        if (!nav.is(":visible") && topDistance < distanceLimit) {
           nav.show();
         } else if (nav.is(":visible") && topDistance > 100) {
           nav.hide();
@@ -67,10 +72,10 @@ $(document).ready(function() {
 
         // on tablet, hide the navigation icon as well and show a "scroll to top
         // icon" instead
-        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
+        if (!$("#menu-icon").is(":visible") && topDistance < 50) {
           $("#menu-icon-tablet").show();
           $("#top-icon-tablet").hide();
-        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
+        } else if (!$("#menu-icon").is(":visible") && topDistance > 100) {
           $("#menu-icon-tablet").hide();
           $("#top-icon-tablet").show();
         }
@@ -81,12 +86,12 @@ $(document).ready(function() {
      * Show mobile navigation menu after scrolling upwards,
      * hide it again after scrolling downwards.
      */
-    if ($( "#footer-post").length) {
+    if ($("#footer-post").length) {
       var lastScrollTop = 0;
-      $(window).on("scroll", function() {
+      $(window).on("scroll", function () {
         var topDistance = $(window).scrollTop();
 
-        if (topDistance > lastScrollTop){
+        if (topDistance > lastScrollTop) {
           // downscroll -> show menu
           $("#footer-post").hide();
         } else {
